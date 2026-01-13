42f34cf804ee5ade448c9c4def3dc071b3b1d0d2
maverick2701 <46095355+maverick2701@users.noreply.github.com>
2026-01-12 23:50:56 -0600

    Added examples of O(n*n) and O(1)

--- data structures & algorithms/o(n).py
+++ data structures & algorithms/o(n).py
@@ -1,4 +1,4 @@
-#Big O: n, 2n, n squared, log(n)
+# Big O: n, 2n, n squared, 1, log(n)
 
 # O(n)
 def print_items_order_n(n):
@@ -18,7 +18,22 @@ def print_items_order_n_squared(n):
         for j in range(n):
             print(i, j)
      
+# O(n squared + n) - drop non-dominants
+def print_items_order_n_squared_plus_n(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+    
+    for k in range(n):
+        print(k)
+
+# O(1)
+def add_items(n):
+    print(n + n)
+
 
 print_items_order_n(10)    
 print_items_order_2n(10)
 print_items_order_n_squared(10)
+print_items_order_n_squared_plus_n(10)
+add_items(10)
