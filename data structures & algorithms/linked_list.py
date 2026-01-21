600a1d95b5bad27879647d34cc1a876dcd71d5d1
maverick2701 <46095355+maverick2701@users.noreply.github.com>
2026-01-20 22:55:07 -0600

    Renamed ll-constructor to linked_list

--- data structures & algorithms/linked_list.py
+++ data structures & algorithms/linked_list.py
@@ -46,6 +46,30 @@ class LinkedList:
         self.length -= 1
         #print(f"Tail: {self.tail.value}")
         return temp.value
+    
+    def prepend(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+        self.length += 1
+
+    def pop_first(self):
+        if self.length == 0:
+            print("Nothing to pop!!!")
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+
+        self.length -= 1
+        return temp.value
+
+
         
 
 my_ll = LinkedList(25)
@@ -53,12 +77,19 @@ print(f"Head: {my_ll.head.value}")
 my_ll.append(36)
 my_ll.append(49)
 my_ll.append(64)
-#my_ll.append(81)
+my_ll.append(81)
 my_ll.print_list()
 print(my_ll.pop())
 print(my_ll.pop())
+#print(my_ll.pop())
+my_ll.prepend(121)
+my_ll.prepend(144)
+my_ll.print_list()
+my_ll.pop_first()
 print(my_ll.pop())
 print(my_ll.pop())
+my_ll.pop_first()
+my_ll.print_list()
 print(my_ll.pop())
-# my_ll.append(100)
-#my_ll.print_list()
+my_ll.print_list()
+my_ll.pop_first()
